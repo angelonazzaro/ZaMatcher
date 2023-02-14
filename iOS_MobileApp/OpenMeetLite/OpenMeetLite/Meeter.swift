2c9ca8d4cfd61edaf3bdcc2d202f6fe7a84cb86c
Yuri Brandi <destroyercrazy0@gmail.com>
2023-02-14 02:53:19 +0100

    Added Meeter struct with data storage and retrieval

--- iOS_MobileApp/OpenMeetLite/OpenMeetLite/Meeter.swift
+++ iOS_MobileApp/OpenMeetLite/OpenMeetLite/Meeter.swift
@@ -2,17 +2,54 @@
 //  Meeter.swift
 //  OpenMeetLite
 //
-//  Created by Yuri on 08/02/23.
+//  Created by Yuri on 14/02/23.
 //
 
 import Foundation
 
-/*class Meeter: Codable{
-    @objc dynamic var Id:Int
-    @objc dynamic var Name:String
-    @objc dynamic var Surname:String
-    @objc dynamic var City:String
-    @objc dynamic var Gender:String
-    @objc dynamic var SearchingGender:String
-    @objc dynamic var Hobbies:
-}*/
+struct Meeter: Codable {
+    var name: String
+    var surname: String
+    var gender: String
+    var searchingGender: String
+    var age: Int
+    var interests: String
+    var latitude: Double
+    var longitude: Double
+}
+
+
+func doStoreMeeter(meeter: Meeter){
+    do {
+        // Create JSON Encoder
+        let encoder = JSONEncoder()
+
+        // Encode Note
+        let data = try encoder.encode(meeter)
+
+        // Write/Set Data
+        UserDefaults.standard.set(data, forKey: "user")
+
+    } catch {
+        print("Unable to Encode Note (\(error))")
+    }
+
+}
+
+func doRetrieveMeeter() -> Meeter{
+    if let data = UserDefaults.standard.data(forKey: "user") {
+        do {
+            // Create JSON Decoder
+            let decoder = JSONDecoder()
+
+            // Decode Note
+            let meeter = try decoder.decode(Meeter.self, from: data)
+            return meeter
+
+        } catch {
+            print("Unable to Decode Note (\(error))")
+        }
+    }
+    
+    return Meeter(name: "", surname: "", gender: "", searchingGender: "", age: 0, interests: "", latitude: 0.0, longitude: 0.0)
+}
